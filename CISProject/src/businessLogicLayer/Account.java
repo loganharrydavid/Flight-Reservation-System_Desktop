5e45d45cc0e23cc9c0df37383842d10b8298f504
Jordan Lightgate <Jordan Lightgate@HarryGinglesDulaneyTheFourth>
2019-04-05 19:46:25 -0400

    added Account class

--- CISProject/src/businessLogicLayer/Account.java
+++ CISProject/src/businessLogicLayer/Account.java
@@ -1,5 +1,124 @@
 package businessLogicLayer;
 
-public class Account {
+public class Account implements Comparable<Account> {
+	private String firstName;
+	private String lastName;
+	private String userName;
+	private String address;
+	private String email;
+	private String password;
+	private String securityQuestion;
+	private String state;
+	private int zipCode;
+	private int ssn;
+	private String flightInfo;
 
+	public Account() {
+
+	}
+
+	public Account(String firstname, String lastName, String userName,
+			String address, String email, String password, String securityQuestion, String State,
+			int zipCode, int ssn, String flightInfo) {
+		
+	}
+		
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getSecurityQuestion() {
+		return securityQuestion;
+	}
+
+	public void setSecurityQuestion(String securityQuestion) {
+		this.securityQuestion = securityQuestion;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public int getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(int ssn) {
+		this.ssn = ssn;
+	}
+
+	public String getFlightInfo() {
+		return flightInfo;
+	}
+
+	public void setFlightInfo(String flightInfo) {
+		this.flightInfo = flightInfo;
+	}
+	
+	@Override
+	public int compareTo(Account a) {
+		if(this.ssn == a.ssn) {	
+			return 0;
+		}else
+		return -1;		
+	}
+	
+	
 }

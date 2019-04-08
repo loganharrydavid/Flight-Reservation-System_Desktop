e2da418994bd1cc08817c5978965dd82b127fc39
khantlin <khantlin@10.69.135.140>
2019-04-08 16:28:20 -0400

    New Stuff

--- CISProject/src/graphicUserInterface/Registration.java
+++ CISProject/src/graphicUserInterface/Registration.java
@@ -23,8 +23,9 @@ public class Registration extends Application {
 	Label username;
 	Label email;
 	Label passwords;
+	Label confirmPasswords;
 	Label SSN;
-	Label SequrityQuestion;
+	Label SecurityQuestion;
 	TextField firstNameInput;
 	TextField lastNameInput;
 	TextField addressInput;
@@ -32,9 +33,10 @@ public class Registration extends Application {
 	TextField stateInput;
 	TextField userNameInput;
 	TextField emailInput;
-	PasswordField passwordInput;
+	TextField passwordInput;
+	TextField confirmPasswordInput;
 	TextField SSNInput;
-	TextField SequrityQuestionZInput;
+	TextField SecurityQuestionInput;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -47,34 +49,88 @@ public class Registration extends Application {
 		grid = new GridPane();
 		grid.setAlignment(Pos.CENTER);
 		
+		// First name
 		firstName = new Label("First Name:");
 		GridPane.setConstraints(firstName, 0, 0);
-		
 		firstNameInput = new TextField();
 		firstNameInput.setPromptText(" Enter First Name ");
 		GridPane.setConstraints(firstNameInput, 0, 1);
 		
+		// Last Name
 		lastName = new Label("Last Name:");
 		GridPane.setConstraints(lastName, 0, 2);
-		
 		lastNameInput = new TextField();
 		lastNameInput.setPromptText(" Enter Last Name ");
 		GridPane.setConstraints(lastNameInput, 0, 3);
 		
-		address = new Label(" Address ");
+		// Address
+		address = new Label(" Address: ");
 		GridPane.setConstraints(address, 0, 4);
-	    
 		addressInput = new TextField();
-		addressInput.setPromptText(" Enter Last Name ");
+		addressInput.setPromptText(" Enter the Address ");
 		GridPane.setConstraints(addressInput, 0, 5);
 		
+		// State
+		state = new Label(" State: ");
+		GridPane.setConstraints(state, 0, 6);
+		stateInput= new TextField();
+		stateInput.setPromptText(" Enter the State ");
+		GridPane.setConstraints(stateInput, 0, 7);
+				
+		// Zip Code
+		zipCode = new Label(" Zip Code: ");
+		GridPane.setConstraints(zipCode, 0, 8);
+		zipCodeInput = new TextField();
+		zipCodeInput.setPromptText(" Enter Zip Code ");
+		GridPane.setConstraints(zipCodeInput, 0, 9);
+	
+		// UserName
+		username = new Label("Enter Username:");
+		GridPane.setConstraints(username, 1, 0);
+		userNameInput = new TextField();
+		userNameInput.setPromptText(" Enter Username ");
+		GridPane.setConstraints(userNameInput, 1, 1);
+		
+		//Passwords
+		passwords = new Label(" Password: ");
+		GridPane.setConstraints(passwords, 1, 2);
+		passwordInput = new TextField();
+		passwordInput.setPromptText("Enter you passwords ");
+		GridPane.setConstraints(passwordInput,1, 3);
+		
+		//Confirm Passwords
+		confirmPasswords = new Label(" Confrim Passwords: ");
+		GridPane.setConstraints(confirmPasswords, 1, 4);
+		confirmPasswordInput = new TextField();
+		confirmPasswordInput.setPromptText(" Enter Confirm Password ");
+		GridPane.setConstraints(confirmPasswordInput, 1, 5);
+		
+		// SSN
+		SSN = new Label(" Soical sercurity number: ");
+		GridPane.setConstraints(SSN, 1, 6);
+		SSNInput = new TextField();
+		SSNInput.setPromptText(" Enter SSN ");
+		GridPane.setConstraints(SSNInput, 1, 7 );
+		
+		// Security Question 
+		SecurityQuestion = new Label (" Sequrity Question ");
+		GridPane.setConstraints(SecurityQuestion,1, 8);
+		SecurityQuestionInput = new TextField();
+		SecurityQuestionInput.setPromptText(" Favorite vication place ");
+		GridPane.setConstraints(SecurityQuestionInput, 1, 9);
 		
 		
-		grid.getChildren().addAll(firstName, firstNameInput, lastName, lastNameInput, address, addressInput );
+		
+		grid.getChildren().addAll(firstName, firstNameInput, lastName, lastNameInput, address,
+				                  addressInput, zipCode, zipCodeInput
+				                  ,state, stateInput,username, userNameInput, 
+				                  passwords, passwordInput, confirmPasswords, 
+				                  confirmPasswordInput, SSN, SSNInput,
+				                  SecurityQuestion, SecurityQuestionInput);
 		
 		
 		
-		scene = new Scene(grid, 500,500);
+		scene = new Scene(grid, 600,600);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
--- CISProject/src/graphicUserInterface/SplashScreen2.java
+++ CISProject/src/graphicUserInterface/SplashScreen2.java
@@ -1,60 +1,35 @@
 package graphicUserInterface;
 
 import javafx.application.Application;
-
-import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-
-
+import javafx.stage.Stage;
+/**
+*
+* @author zoranpavlovic.blogspot.com
+*/
 public class SplashScreen2 extends Application {
-	
-// as for next step we will rename the stage window.
-	Stage window;
-	Scene scene1, scene2, scene3;
-
-	public static void main (String[] args) {
-		launch(args); // its call launch method from application class make your class javafx
-	}
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		window = primaryStage;// now we name the stage as window
-
-		Label label1 = new Label(" Welcome to the first place ");// its the chuck of text appear on the window
-		// next we will create button to switch the scenes
-		Button button1 = new Button();
-		button1.setText(" login ");
-		
-		button1.setOnAction(e -> window.setScene(scene2));
-		
-		// we use lambda to switch from on scene to another scene
-		
-		// layout 1 = children are laid out in vertical column;
-		VBox layout1 = new VBox(20); // <- you can also passing the vlaue.
-		// we use VBox layout to stack all the objects top on each others.
-		layout1.getChildren().addAll(label1,button1);
-		// we have multiple stuff to add so we will do add all.
-		Scene scene1 = new Scene(layout1, 700, 700);// we created the scene.
-		
-		// button 2 for the second scene.
-		Button button2 = new Button(" User name Enter ");
-		button2.setOnAction(e -> window.setScene(scene1));
-		// layout2
-		StackPane layout2 = new StackPane();
-		layout2.getChildren().add(button2);
-		scene2 = new Scene (layout2, 600, 600);
-		
-		window.setScene(scene1);
-		window.setTitle(" Welcome !");
-		window.show();
-		
-	}
-	
-
+        /**
+        * @param args the command line arguments
+        */
+        public static void main(String[] args) {
+        Application.launch(args);
+        }
+        @Override
+        public void start(Stage primaryStage) {
+        primaryStage.setTitle("Load Image");
+ 
+        StackPane sp = new StackPane();
+        Image img = new Image("javafx.jpg");
+        ImageView imgView = new ImageView(img);
+        sp.getChildren().add(imgView);
+ 
+        //Adding HBox to the scene
+        Scene scene = new Scene(sp);
+        primaryStage.setScene(scene);
+        primaryStage.show();
 }
+}
+

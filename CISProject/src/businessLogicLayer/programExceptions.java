a4b7d8a7ae6374f8c99270a0bdc5d61286305095
Jordan Lightgate <Jordan Lightgate@HarryGinglesDulaneyTheFourth.hsd1.ga.comcast.net>
2019-04-09 22:10:48 -0400

    Added program exceptions class so we can define our own exceptions to throw

---
+++ CISProject/src/businessLogicLayer/programExceptions.java
@@ -0,0 +1,9 @@
+package businessLogicLayer;
+
+public class programExceptions extends Exception{
+	
+	//this class will define the specific exception objects we will need at various
+	//levels of our program
+	
+
+}

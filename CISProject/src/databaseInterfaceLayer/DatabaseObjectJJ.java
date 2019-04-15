0370897d9668afbb74c5c819bba007c5ec3d0906
davidkelly <davidkelly@10.250.83.220>
2019-04-15 19:38:23 -0400

    Example for somebody. Same code.

--- CISProject/src/databaseInterfaceLayer/DatabaseObjectJJ.java
+++ CISProject/src/databaseInterfaceLayer/DatabaseObjectJJ.java
@@ -10,7 +10,7 @@ public class DatabaseObjectJJ {
 	static String databasePassword = "Redwall12";
 	
 	
-	public static void main(String [] args) {
+	public static void main(String[] args) {
 		
 		DatabaseObjectJJ DBO = new DatabaseObjectJJ();
 		

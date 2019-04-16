838204350243fe828ebf8ed6098e2123d63e3ee6
Jordan Lightgate <Jordan Lightgate@10.250.54.18>
2019-04-16 11:44:57 -0400

    Starting work on Exception Handler, will write exceptions for the
    Registration inputs and login inputs to check their formating..

--- CISProject/src/businessLogicLayer/ExceptionHandler.java
+++ CISProject/src/businessLogicLayer/ExceptionHandler.java
@@ -6,7 +6,11 @@ public class ExceptionHandler extends Exception{
 	
 		
 	}
-	 
+	
+//Duplicate flight exception
+//Duplicate Account exception
+//All input registration input exceptions (check the formating of SSN needs its own, email needs its own, password needs its 
+//owns, but many of the STRING inputs can use the same exception handler ie. firstname,lastname......
 	 
 	
 

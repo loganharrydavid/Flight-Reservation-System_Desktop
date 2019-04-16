feab9c6f2bede81f5f52b4e54fa8fb09b1c96122
Jordan Lightgate <Jordan Lightgate@HarryGinglesDulaneyTheFourth.hsd1.ga.comcast.net>
2019-04-15 23:09:08 -0400

    c

--- CISProject/src/businessLogicLayer/ExceptionHandler.java
+++ CISProject/src/businessLogicLayer/ExceptionHandler.java
@@ -3,23 +3,7 @@ package businessLogicLayer;
 import java.util.InputMismatchException;
 
 public class ExceptionHandler extends Exception{
-	try {
-		if(userAnswer.equalsIgnoreCase(this.securityAnswer)){
-			return this.password;
-		}else {
-				throw new InputMismatchException("Wrong answer");
-				}
-		
-	}
-		catch(InputMismatchException ex) {
-			return ex.getMessage();
-		}
 	
-	
-	
-	
-	
-		
 		
 	}
 	 

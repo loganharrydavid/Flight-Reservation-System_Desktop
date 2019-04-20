4225fe4028db93399932e07253abe149f79d12fd
davidkelly <davidkelly@davids-air.wifi.gsu.edu>
2019-04-20 13:27:26 -0400

    More Updates.

--- CISProject/src/databaseInterfaceLayer/DatabaseObjectJJ.java
+++ CISProject/src/databaseInterfaceLayer/DatabaseObjectJJ.java
@@ -1,7 +1,5 @@
 package databaseInterfaceLayer;
-
 import java.sql.*;
-
 import businessLogicLayer.*;
 import java.util.ArrayList;
 
@@ -11,19 +9,14 @@ public class DatabaseObjectJJ {
 	// rid of the verification error
 	static final String databaseURL = "jdbc:mysql://localhost:3306/JavaJesusDB";
 	static final String databaseUsername = "root";
-	static final String databasePassword = "1234abcd"; //redwall112
+	static final String databasePassword = "1234abcd"; 
 	static Connection connection;
 	public ArrayList<Object> returnList;
 	
 
-	//location of database //set verify certificate to false to not use SSL to get rid of the verification error
+	//location of database set verify certificate to false to not use SSL to get rid of the verification error
 	
 	
-	
-	public static void main(String[] args) { 
-	
-
-	}
 	
 	public DatabaseObjectJJ(ArrayList<Object> list) {
 		
@@ -36,7 +29,7 @@ public class DatabaseObjectJJ {
 		
 				 Class.forName("java.sql.Driver");
 			
-			//System.out.println("database connected! ");
+			System.out.println("database connected! ");
 			
 		
 			
@@ -44,24 +37,26 @@ public class DatabaseObjectJJ {
 
 			connection = DriverManager.getConnection(databaseURL, databaseUsername, databasePassword);
 
-			//String sqlQuery = " INSERT INTO account(username,password,firstname,lastname,address,state,email,zipcode,ssn,security_q,security_a)"
-					//+ " VALUES(?,?,?,?,?,?,?,?,?,?,?);";
+			String sqlQuery = " INSERT INTO Customer_Account(UserName,Password01,FirstName,LastName,Email,Address,City,State,Zipcode,SSN,SecurityQuestion,SecurityAnswer)"
+					+ " VALUES(dav745,1234,David,Kelly,dakell745@gmail.com,135 Edgewood Avenue,City,Georgia,111111111,What college do I go to,Goergia,State);";
 
 			// the prepared statement is more efficient to execute the same or similar
 			// database statement repeatedly
 			PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
 
-			preparedStatement.setString(1, acc.getUserName());
-			preparedStatement.setString(2, acc.getPassword());
-			preparedStatement.setString(3, acc.getFirstName());
-			preparedStatement.setString(4, acc.getLastName());
-			preparedStatement.setString(5, acc.getAddress());
-			preparedStatement.setString(6, acc.getState());
-			preparedStatement.setString(7, acc.getEmail());
-			preparedStatement.setInt(8, acc.getZipCode());
-			preparedStatement.setInt(9, acc.getSsn());
-			preparedStatement.setString(10, acc.getSecurityQuestion());
-			preparedStatement.setString(11, acc.getSecurityAnswer());
+			preparedStatement.setString(1, account.getUserName());
+			preparedStatement.setString(2, account.getPassword());
+			preparedStatement.setString(3, account.getFirstName());
+			preparedStatement.setString(4, account.getLastName());
+			preparedStatement.setString(3, account.getEmail());
+			preparedStatement.setString(5, account.getAddress());
+			//preparedStatement.setString(6, account.getCity());
+			preparedStatement.setString(6, account.getState());
+			preparedStatement.setString(7, account.getEmail());
+			preparedStatement.setInt(8, account.getZipCode());
+			preparedStatement.setInt(9, account.getSsn());
+			preparedStatement.setString(10, account.getSecurityQuestion());
+			preparedStatement.setString(11, ccount.getSecurityAnswer());
 
 			preparedStatement.executeUpdate();
 			

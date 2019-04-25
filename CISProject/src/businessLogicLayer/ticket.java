b65a10329a0045d38c44f9b258831b2c85dd0214
H.G.D. IV <20808723+Nothingrhymeswithorange@users.noreply.github.com>
2019-04-25 16:08:56 -0400

    attempting merge conflict fix

---
+++ CISProject/src/businessLogicLayer/ticket.java
@@ -0,0 +1,17 @@
+package businessLogicLayer;
+
+import java.util.ArrayList;
+
+public class ticket {
+	
+	private int ticketNumber;
+	private Flight flight;
+	private ArrayList<String>account_id = new ArrayList<>();
+	
+public ticket() {
+	
+}
+public ticket(int ticketNumber, Flight flight) {
+	
+}
+}
